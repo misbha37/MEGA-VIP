3b1edbaf7157bc1cdc6ea7e866bfc6d15d8d67d9
MAYANK UL HAQ <misbhaulhaq865@gmail.com>
2025-02-11 21:28:18 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'shabankhan3788@gmail.com',
-    password: 'shabankhan3788@',
+    email: 'womebi6670@minduls.com',
+    password: 'womebi6670@',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
